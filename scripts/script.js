abfe27559b3db9b709c04682383251ddf86549bb
fckolob <131497920+fckolob@users.noreply.github.com>
2026-01-07 21:39:19 -0300

    course title generated in a dynamic way

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,4 @@
 let lastModified = document.getElementById("last-modified");
 lastModified.textContent = `Last Modified: ${document.lastModified}`;
+let courseTitle = document.getElementById("course-title");
+courseTitle.textContent = "WDD 131 Dynamic Web Fundamentals";
