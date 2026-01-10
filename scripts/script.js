0421156204acd363c9963ce2d795f4f58f2fd64f
fckolob <131497920+fckolob@users.noreply.github.com>
2026-01-10 20:58:47 -0300

    copyright year generated in a dynamic way

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,5 @@ let lastModified = document.getElementById("last-modified");
 lastModified.textContent = `Last Modified: ${document.lastModified}`;
 let courseTitle = document.getElementById("course-title");
 courseTitle.textContent = "WDD 131 Dynamic Web Fundamentals";
+let copyrightYear = document.getElementById("copyright-year");
+copyrightYear.innerHTML = `Fernando Caballero &copy; ${new Date().getFullYear()} Uruguay`;
