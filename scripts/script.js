18888f186624e70dd3619a3c6c7be703c9046de0
fckolob <131497920+fckolob@users.noreply.github.com>
2026-01-10 21:40:20 -0300

    title fixed

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    
-    const courseTitle = document.getElementById("course-title");
-    if (courseTitle) {
-        courseTitle.innerHTML = "WDD 131 Dynamic Web Fundamentals";
-    }
-
-    
-});
