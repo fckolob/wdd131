b13681a2c8eb7082b5d8e659802f6aa4e4ed3fe0
fckolob <131497920+fckolob@users.noreply.github.com>
2026-01-10 21:12:14 -0300

    title fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,16 @@
-let lastModified = document.getElementById("last-modified");
-lastModified.textContent = `Last Modified: ${document.lastModified}`;
-let courseTitle = document.getElementById("course-title");
-courseTitle.textContent = "WDD 131 Dynamic Web Fundamentals";
-let copyrightYear = document.getElementById("copyright-year");
-copyrightYear.innerHTML = `Fernando Caballero &copy; ${new Date().getFullYear()} Uruguay`;
+document.addEventListener("DOMContentLoaded", () => {
+    const lastModified = document.getElementById("last-modified");
+    if (lastModified) {
+        lastModified.textContent = `Last Modified: ${document.lastModified}`;
+    }
+
+    const courseTitle = document.getElementById("course-title");
+    if (courseTitle) {
+        courseTitle.innerHTML = "WDD 131 Dynamic Web Fundamentals";
+    }
+
+    const copyrightYear = document.getElementById("copyright-year");
+    if (copyrightYear) {
+        copyrightYear.innerHTML = `Fernando Caballero &copy; ${new Date().getFullYear()} Uruguay`;
+    }
+});
