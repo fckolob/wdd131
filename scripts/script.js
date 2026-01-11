afd9a609b353f743959df20176e224b82ece61cc
fckolob <131497920+fckolob@users.noreply.github.com>
2026-01-10 21:28:08 -0300

    scripts and nav fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const lastModified = document.getElementById("last-modified");
-    if (lastModified) {
-        lastModified.textContent = `Last Modified: ${document.lastModified}`;
-    }
-
+    
     const courseTitle = document.getElementById("course-title");
     if (courseTitle) {
         courseTitle.innerHTML = "WDD 131 Dynamic Web Fundamentals";
     }
 
-    const copyrightYear = document.getElementById("copyright-year");
-    if (copyrightYear) {
-        copyrightYear.innerHTML = `Fernando Caballero &copy; ${new Date().getFullYear()} Uruguay`;
-    }
+    
 });
